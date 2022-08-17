071d2829587f82e8d0ead51f18c620ea82831a78
Phan Nguyen <nguyenngocphan36@gmail.com>
2022-08-17 14:14:01 +0700

    _:change field name

--- quickfixj-core/src/main/java/quickfix/custom/ThirdPartyBroker.java
+++ quickfixj-core/src/main/java/quickfix/custom/ThirdPartyBroker.java
@@ -2,17 +2,17 @@ package quickfix.custom;
 
 import quickfix.StringField;
 
-public class ThirdPartySource extends StringField {
+public class ThirdPartyBroker extends StringField {
 
 	private static final long serialVersionUID = 2146466373360979152L;
 
 	public static final int FIELD = 6002;
 
-	public ThirdPartySource() {
+	public ThirdPartyBroker() {
 		super(FIELD);
 	}
 
-	public ThirdPartySource(final String data) {
+	public ThirdPartyBroker(final String data) {
 		super(FIELD, data);
 	}
 
